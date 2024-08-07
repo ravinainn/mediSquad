30b3df2188808a26742761a0a3975d122914227c
Shubham <shxxxam@gmail.com>
2024-08-07 23:33:42 +0530

    initial commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../components/Home/Hero";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div>
       <h1>MediSquad</h1>
       <Hero />
+      <Footer />
     </div>
   );
 };
