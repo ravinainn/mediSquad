6ef393e5178f6c115a6c15e8a7bc8f37356caf5f
ravinainn <nain.ravi12345@gmail.com>
2024-08-07 22:06:14 +0530

    created new file

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Hero from "../components/Home/Hero";
 
 const Home = () => {
-  return <div>MEDISQUAD</div>;
+  return (
+    <div>
+      <h1>MediSquad</h1>
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
