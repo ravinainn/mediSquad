0ffcdb08947d3945be7781af35bd8508a0c441de
Shubham <shxxxam@gmail.com>
2024-08-09 16:38:02 +0530

    modified profile

--- client/src/components/consult/ProfileDashboard.jsx
+++ client/src/components/consult/ProfileDashboard.jsx
@@ -13,10 +13,7 @@ const ProfileDashboard = ({ onClose }) => {
           </div>
           <div className="mt-4 flex flex-col space-y-2">
             <button className="px-4 py-2 bg-blue-400 text-white text-base font-medium rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
-              Edit Profile
-            </button>
-            <button className="px-4 py-2 bg-teal-400 text-white text-base font-medium rounded-md shadow-sm hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-green-300">
-              Account Settings
+              Dashboard
             </button>
             <button className="px-4 py-2 bg-red-400 text-white text-base font-medium rounded-md shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-300">
               Logout
