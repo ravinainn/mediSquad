bfeb06d163233f8f79baa76a9c7f66eba684fca0
ravinainn <nain.ravi12345@gmail.com>
2024-08-09 14:45:07 +0530

    backend removed

--- server/src/models/Admin.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import User from "./User";
-
-const adminSchema = new mongoose.Schema({});
-
-const Admin = User.discriminator("Admin", adminSchema);
-
-export default Admin;
